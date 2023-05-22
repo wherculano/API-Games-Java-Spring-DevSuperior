e5a6ed2cb40caed0e9076ffdfdae06cbcf3caca4
Wagner Herculano <wagherculano@hotmail.com>
2023-05-22 11:09:03 -0300

    Added Game DTO

---
+++ src/main/java/com/minicurso/devsuperior/dslist/dto/GameMinDTO.java
@@ -0,0 +1,43 @@
+package com.minicurso.devsuperior.dslist.dto;
+
+
+import com.minicurso.devsuperior.dslist.entities.Game;
+
+public class GameMinDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO() {
+    }
+
+    public GameMinDTO(Game entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.year = entity.getYear();
+        this.imgUrl = entity.getImgUrl();
+        this.shortDescription = entity.getShortDescription();
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Integer getYear() {
+        return this.year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+}
